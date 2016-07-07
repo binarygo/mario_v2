8697d7723fb043180bb441e305a565038e19ab89
Shuo Chen <chausolaris@gmail.com>
2016-07-07 08:05:52 -0400

    adjust parameters.

--- bot_main.lua
+++ bot_main.lua
@@ -4,16 +4,20 @@ require "mario_uct_model"
 
 local function mario_uct_model_main()
   local model = mario_uct_model.UctModel:new()
+  
   model.save_to = "uct_model.sav"
   model.log_file = io.open("uct_model.log", "a")
+
   model.result_actions = {}
-  model.all_actions = {
-    0x00, 0x04, 0x01
-  }
-  model.min_num_visits_to_expand_node = 2
-  model.max_num_runs = 5
-  model.max_depth = 10
+  model.all_actions = {}
+  model.num_skip_frames = 12
+
+  model.min_num_visits_to_expand_node = 1
+  model.max_num_runs = 100
+  model.max_depth = 60
+  
   model.enable_debug = true
+
   model:main()
 end
 
